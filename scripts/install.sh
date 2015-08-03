61b6dbe3defb95054fe3640ddf3023c58bfeb91f
Loetkolben <SirLoetkolben@gmail.com>
2015-08-03 18:10:06 +0200

    We need sudo... Ups...

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ TS3_URL="http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd6
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get -y install curl
+apt-get -y install curl sudo
 apt-get clean
 
 curl -Ls "$TS3_URL" | tar zxf - -C /opt
