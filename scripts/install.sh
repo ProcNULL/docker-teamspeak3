3393fbdd9d0274dbc45e55ebdcd09e79ea3c2916
Loetkolben <SirLoetkolben@gmail.com>
2015-08-17 10:32:04 +0200

    Update TS3 to v3.0.11.3

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TS3_URL="http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz"
+TS3_URL="http://dl.4players.de/ts/releases/3.0.11.3/teamspeak3-server_linux-amd64-3.0.11.3.tar.gz"
 
 export DEBIAN_FRONTEND=noninteractive
 
